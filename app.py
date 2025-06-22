97ed164bc4d62007e01c0d9b62be6a4c266a1217
dheeraz15 <dhirajchapagain353@gmail.com>
2025-06-22 21:28:49 +0545

    prometheus_client

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
 from flask import Flask
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 
-@app.route('/hello') 
+# Define a counter for each route
+hello_counter = Counter('hello_requests_total', 'Total /hello requests')
+hi_counter = Counter('hi_requests_total', 'Total /hi requests')
+yes_counter = Counter('yes_requests_total', 'Total /yes requests')
+
+@app.route('/hello')
 def hello():
+    hello_counter.inc()
     return "Hello, DevOps!"
 
-
-@app.route('/hi') 
+@app.route('/hi')
 def hi():
+    hi_counter.inc()
     return "Hi!"
 
 @app.route('/yes')
 def yes():
+    yes_counter.inc()
     return "Yes!"
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': 'text/plain; version=0.0.4; charset=utf-8'}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
 
-#hi
